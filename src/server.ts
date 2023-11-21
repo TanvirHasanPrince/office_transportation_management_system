e34aec5483e8d057b77025ead1e0d2d75c71d6a2
unknown <t.hasan.prince@gmail.com>
2023-11-21 16:30:23 +0600

    WARNING- Deployed version to vercel-Does not contain errorLogger

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
 import mongoose from 'mongoose';
 import config from './config/index';
 import app from './app';
-import { errorlogger, logger } from './shared/logger';
+// import { errorlogger, logger } from './shared/logger';
 import { Server } from 'http';
 
 process.on('uncaughtException', error => {
-  errorlogger.error('uncaughtException detected', error);
+  console.error('uncaughtException detected', error);
 
   process.exit(1);
 });
@@ -15,13 +15,13 @@ let server: Server;
 async function main() {
   try {
     await mongoose.connect(config.database_url as string);
-    logger.info(`🛢Database is connected successfully`);
+    console.info(`🛢Database is connected successfully`);
 
     server = app.listen(config.port, () => {
-      logger.info(`Application  listening on port ${config.port}`);
+      console.info(`Application  listening on port ${config.port}`);
     });
   } catch (err) {
-    errorlogger.error('Failed to connect to MongoDB', err);
+    console.error('Failed to connect to MongoDB', err);
   }
 }
 
@@ -30,7 +30,7 @@ main();
 process.on('unhandledRejection', error => {
   if (server) {
     server.close(() => {
-      errorlogger.error(error);
+      console.error(error);
     });
   } else {
     process.exit(1);
@@ -38,7 +38,7 @@ process.on('unhandledRejection', error => {
 });
 
 process.on('SIGTERM', () => {
-  logger.info('SIGTERM is received');
+  console.info('SIGTERM is received');
 
   if (server) {
     server.close();
